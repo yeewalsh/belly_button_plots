56890402780a62398bc1324461036f881647920a
Ellye Walsh <ellyewalsh@gmail.com>
2022-05-30 23:10:39 -0400

    Adding final Challenge assignment

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# belly_button_plots
+# Plotting Belly Button Bacteria Data
 Plotting data on belly button bacteria with javascript and plotly.
+
+## Overview
+In an attempt to find a good source of bacteria for use in alternative-meat products, bacteria was sampled from human belly buttons and analyzed to see the size and content of the bacteria samples. 
+
+Bootstrap elements used for customization:
+* Image applied to jumbotron
+* Navigation bar used to jump to various charts
+* Site optimzed for mobile devises
+
+## Analysis
+Visit the site at: 
+
+Below the navigation bar, you can click the dropdown for "Test Subject ID No.:" to choose a different subject from the dataset. Review the "Demographic Info" to learn more about the individual's age, ethnicity, gender and location. 
+
+The bar chart, "Top 10 Bacteria Cultures Found", shows the top 10 bacterias identified in the sample individual selected. The bacteria types are arranged in descending order and you can hover over them to see the sample size and names included. 
+
+The "Belly Button Washing Frequency" gauge chart visualizes how frequent the selected test subject washes their belly button per week. 
+
+The bubble chart "Bacteria Cultures Per Sample" illustrates the type of bacteria found and the sample size, exaggerated by size of the bubble. 
+
+## Summary
+
+This site provides a useful resource to the researchers in their quest to find good bacteria sources for their alernative-meat produts. Analysis would include clicking through the various subjects to see how prevelent different types of bacteria are, and if there are similar demographic instances for the subjects who exibit a bacteria sample of interest. 
+
+Further analysis would be to review the number of scrubs per week compared to the sample sizes and types to see if the frequency of washing produces more or less varied bacteria samples. 
+
+The bubble chart is only mapping 2 variables: UTO id number on the x-axis and color scheme, and sample size on the y-axis and bubble size. Since each variable is displayed twice, it negates the purpose of the bubble chart, even though it is pretty and fun to look at.
--- charts.js
+++ charts.js
@@ -27,7 +27,7 @@ function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildMetadata(newSample);
   buildCharts(newSample);
-  
+
 }
 
 // Demographics Panel 
@@ -57,11 +57,19 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
-    // 3. Create a variable that holds the samples array. 
+    // 3. Create a variable that holds the samples array.
     var samples = data.samples;
+    
     console.log(samples);
+
+    // 3.5 Create a variable that filters the wfreq from the metadata for the desired sample number. 
+    var metadata = data.metadata.filter(sampleObj => sampleObj.id == sample);
+    var wFreq = parseFloat(metadata[0].wfreq);
+    console.log(wFreq);
+    
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     var newSample = samples.filter(sampleObj => sampleObj.id == sample);
+    
     //  5. Create a variable that holds the first sample in the array.
     var firstSample = newSample[0]
     console.log(firstSample);
@@ -76,20 +84,20 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otu_id.slice(0, 10).map(otu_id => "OTU" + otu_id).reverse();
-    var otu_sliced_reveresed = otu_id.slice(0, 10).reverse();
+    var yticks = otu_id.slice(0, 10).map(otu_id => "OTU " + otu_id).reverse();
+    var otu_sliced_reversed = otu_id.slice(0, 10).reverse();
     var sample_value_sliced_reversed = sample_value.slice(0, 10).reverse();
     var otu_label_sliced_reversed = otu_label.slice(0, 10).reverse();
 
     console.log(yticks)
     // 8. Create the trace for the bar chart. 
-      var trace = {
-        x: sample_value_sliced_reversed,
-        y: yticks,
-        text: otu_label_sliced_reversed,
-        type: "bar",
-        orientation: "h"
-      }
+    var trace = {
+      x: sample_value_sliced_reversed,
+      y: yticks,
+      text: otu_label_sliced_reversed,
+      type: "bar",
+      orientation: "h"
+    }
 
     var barData = [trace];
 
@@ -103,39 +111,79 @@ function buildCharts(sample) {
     Plotly.newPlot("bar", barData, barLayout);
 
     // Deliverable 2 Bubble chart:
-     // 1. Create the trace for the bubble chart.
-     var trace1 = {
-       x: sample_value_sliced_reversed,
-       y: yticks,
-       text: otu_label_sliced_reversed,
-       mode: 'markers',
-       marker: {
-         color: otu_sliced_reveresed,
-         colorscale: 'Earth',
-         sizeref: 0.05,
-         sizemode: 'area'
-       },
-       hoverinfo: "x+y+text"
+    // 1. Create the trace for the bubble chart.
+    var trace1 = {
+      x: otu_id,
+      y: sample_value,
+      text: otu_label,
+      mode: 'markers',
+      marker: {
+        color: otu_id,
+        colorscale: 'Earth',
+        size: sample_value,
+        // sizeref: 0.05,
+        // sizemode: 'area'
+      },
+      hoverinfo: "x+y+text"
 
-     }
-     
-     var bubbleData = [trace1];
+    };
+
+    var bubbleData = [trace1];
 
     // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
       title: "Bacteria Cultures Per Sample",
-      xlabel: "UTO ID",
+      xaxis: { zeroline: true, title: "UTO ID" },
+      // yaxis: {
+      //   autorange : true,
+      //   autotick: false,
+      //   tick0: 0,
+      //   dtick: 50,
+      // },
       margin: {
         l: 20,
         r: 20,
-        b: 20,
-        t: 20,
+        b: 25,
+        t: 25,
         pad: 5
       },
       hovermode: "closest"
     };
 
     // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+    // Deliverable 3: Gauge Chart
+    // 4. Create the trace for the gauge chart.
+    var trace3 = {
+      type: "indicator",
+      mode: "gauge+number",
+      value: wFreq,
+      title: "Scrubs per Week",
+      gauge: {
+        axis: {range: [null, 10]},
+        bar: {color: "black"},
+        steps: [
+          {range: [0, 2], color: "red"},
+          {range: [2, 4], color: "orange"},
+          {range: [4, 6], color: "yellow"},
+          {range: [6, 8], color: "lightgreen"},
+          {range: [8, 10], color: "green"}
+      ]}
+    };
+
+
+    var gaugeData = [trace3];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      title: "Belly Button Washing Frequency"     
+      
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
+
+
 }
